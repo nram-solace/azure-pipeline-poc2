c52ee8246ea167e81e1cea63f6bb2cf8caee48b4
Ramesh Natarajan <rnatara@gmail.com>
2021-06-24 08:21:30 -0500

    checking in inline pipeline yml

---
+++ scripts/provision-queues2.py
@@ -0,0 +1,134 @@
+# provison-queue.py 
+# provision list of queues on Solace brokers
+# if queue already exists, update their properties instead
+#
+# To trigger this from Azure devops pipeline, add this to pipeline yaml:
+#   - stage: provisionQueue
+#     jobs:
+#     - job: provisionQueue
+#       steps:
+#       - task: PythonScript@0
+#         inputs:
+#           scriptSource: 'filePath' # Options: filePath, inline
+#           scriptPath: scripts/provision-queue.py
+#           arguments: --host $(HOST) --user $(SEMPUSER) --pass $(SEMPPASS)
+# and have the variables HOST, SEMPUSER, SEMPPASS defined in the pipeline / variable group
+#
+# Requires:
+#  input/queues.yaml
+#     See input/sample-queues.yaml 
+#  templates/queues.json 
+#
+# Jun 21, 2021 - Ramesh Natarajan (nram), Solace PSG
+
+import sys
+import argparse
+import yaml
+import json
+import requests
+from configparser import ConfigParser
+import string
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+template_json_file="template/queues.json"
+config_url='SEMP/v2/config'
+
+def main(argv):
+    print ('provision-queue.py : starting ')
+
+    p = argparse.ArgumentParser()
+    p.add_argument('--inventory', '-i', dest="inventory_file", required=True, help='Ansible style inventory file') 
+    p.add_argument('--input-file','-f', dest="input_file", required=True, help='input data file') 
+
+
+    r = p.parse_args()
+
+    # read inventory
+    inv = read_config_ini(r.inventory_file)
+    pp.pprint(inv)
+
+    # read template json
+    with open(template_json_file, "r") as fp:
+        template_data = json.load(fp)
+    print ('Template JSON :', type(template_data), template_data)
+
+    # read input yaml file
+    with open(r.input_file, 'r') as f:
+        queues = yaml.load(f)
+    print ('Queues YAML :', type(queues), queues)
+
+    # add template defaults to input yaml
+    for k,v in queues['defaults'].items():
+        template_data[k] = v
+
+def provision_queues(sempurl, user, passwd, queues, template_data):
+
+    vpn = queues['defaults']['msgVpnName']
+    url = f'{sempurl}/{config_url}/msgVpns/{vpn}/queues'
+
+    for queue in queues['queues']:
+        q_url = f'{url}/{queue}'
+        status = get_queue(q_url, r.user, r.passwd)
+        print (f'Get queue {queue} returned {status}')
+        data = template_data.copy()
+        data['queueName'] = queue
+        if status == 200:
+            print (f'Queue {queue} exists. Update settings instead of creating')
+            print (f'Shutting down queue {queue} before updates')
+            # shutdown queue before making changes
+            temp_data = data.copy()
+            temp_data['ingressEnabled'] = False
+            temp_data['egressEnabled'] = False
+            update_queue(q_url, user, passwd, temp_data)
+
+            # send update properties now
+            print (f'Updating queue {queue}')
+            update_queue(q_url, user, passwd, data)
+        else:
+            print (f'Queue does not exist. Create queue {queue}')
+            create_queue(url, user, passwd, data)
+
+def read_config_ini (file, config={}):
+    cfg = config.copy()
+    cp = ConfigParser()
+    cp.read(file)
+    for sec in cp.sections():
+        name = sec.lower()
+        for opt in cp.options(sec):
+            cfg[name + "." + opt.lower()] = cp.get(sec, opt).strip()
+    return cfg
+
+def get_queue(url, user, passwd):
+    print (f'get_queue: url = {url}')
+    status = getattr(requests, 'get')(url, 
+        headers={"content-type": "application/json"},
+        auth=(user, passwd),
+        data=(None))
+    #print('response :\n---\n', status.text)
+    return status.status_code    
+
+def create_queue(url, user, passwd, json_data):
+    print (f'create_queue: url = {url}')
+    #print ('request json:\n---\n', json.dumps(json_data, indent=4))
+    status = getattr(requests, 'post')(url, 
+        headers={"content-type": "application/json"},
+        auth=(user, passwd),
+        data=(json.dumps(json_data)))
+    #print('response :\n---\n', status.text)
+    assert status.status_code == 200, status.text
+    return status.status_code  
+
+def update_queue(url, user, passwd, json_data):
+    print (f'update_queue: url = {url}')
+    #print ('request json:\n---\n', json.dumps(json_data, indent=4))
+    status = getattr(requests, 'patch')(url, 
+        headers={"content-type": "application/json"},
+        auth=(user, passwd),
+        data=(json.dumps(json_data)))
+    #print('response :\n---\n', status.text)
+    assert status.status_code == 200, status.text
+    return status.status_code
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
